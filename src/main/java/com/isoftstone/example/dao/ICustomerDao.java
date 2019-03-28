73453288fc0e8a46fd775400ab7027a481397b58
喻召召 <zzyuf@isoftstone.com>
2019-03-28 20:13:27 +0800

    增加注释

--- src/main/java/com/isoftstone/example/dao/ICustomerDao.java
+++ src/main/java/com/isoftstone/example/dao/ICustomerDao.java
@@ -8,11 +8,21 @@ import org.springframework.data.repository.query.Param;
 
 public interface ICustomerDao extends CrudRepository<Customer,String> {
 
+    /**
+     * 自定义SQL
+     * @param id
+     * @param lastName
+     */
     @Query("update Customer set lastName=:lastName where id=:id ")
     @Modifying
     void updateCustomerById(@Param("id") String id, @Param("lastName") String lastName);
 
 
+    /**
+     * 自定义接口，由JPA帮我们实现
+     * @param id
+     * @return
+     */
     Customer getCustomerById(@Param("id")String id);
 
 }
